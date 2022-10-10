4323c538df56b0705e57f6194058eed0b6883212
Vincent Lambert <v.lambert@eurelis.com>
2022-10-10 14:06:10 +0200

    Support for PHP 8.0 and Xdebug 3.0

--- ubuntu-php8/push.sh
+++ ubuntu-php8/push.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+docker build --build-arg PHP_VERSION=8.0 --build-arg XDEBUG_VERSION=3.0.0 -t ubuntu-php8:php80 .
+docker tag ubuntu-php8:php80 eurelis/ubuntu-php8:php80
+docker push eurelis/ubuntu-php8:php80
+
 docker build --build-arg PHP_VERSION=8.1 --build-arg XDEBUG_VERSION=3.1.0 -t ubuntu-php8:php81 .
-docker login
 docker tag ubuntu-php8:php81 eurelis/ubuntu-php8:php81
 docker push eurelis/ubuntu-php8:php81

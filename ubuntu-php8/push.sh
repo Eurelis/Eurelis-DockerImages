58ca24fe43705919784a29a4fe72ccb61154ae61
Antoine Pineau <a.pineau@eurelis.com>
2024-08-27 11:51:22 +0200

    update creation of image php8.3

--- ubuntu-php8/push.sh
+++ ubuntu-php8/push.sh
@@ -11,3 +11,7 @@ docker push eurelis/ubuntu-php8:php81
 docker build --build-arg PHP_VERSION=8.2 --build-arg XDEBUG_VERSION=3.3.0 -t ubuntu-php8:php82 .
 docker tag ubuntu-php8:php82 eurelis/ubuntu-php8:php82
 docker push eurelis/ubuntu-php8:php82
+
+docker build --build-arg PHP_VERSION=8.3 --build-arg XDEBUG_VERSION=3.3.2 -t ubuntu-php8:php83 .
+docker tag ubuntu-php8:php83 eurelis/ubuntu-php8:php83
+docker push eurelis/ubuntu-php8:php83
